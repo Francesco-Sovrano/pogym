35ca43c3863ced8436c602429a0d870dcabe983f
Steven Morad <sm2558@cam.ac.uk>
2022-06-05 18:12:29 +0100

    init

--- pogym/__init__.py
+++ pogym/__init__.py
@@ -3,7 +3,7 @@ import inspect
 from pogym.envs.blackjack import BlackJack
 from pogym.envs.higher_lower import HigherLower
 
-ENVS = {
+ALL_ENVS = {
         BlackJack: {
             "id": "pogym-Blackjack-v0",
         },
@@ -12,7 +12,7 @@ ENVS = {
         },
 }
 
-for e, v in ENVS.items():
+for e, v in ALL_ENVS.items():
     gym.envs.register(
         entry_point=":".join([inspect.getmodule(e).__name__, e.__name__]),
         **v
