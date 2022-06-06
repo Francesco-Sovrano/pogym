24c466a2f5d42d12dfffa7f506cf76d10ae2c70f
Steven Morad <sm2558@cam.ac.uk>
2022-06-06 11:54:09 +0100

    Add control tasks to init

--- pogym/__init__.py
+++ pogym/__init__.py
@@ -2,18 +2,21 @@ import gym
 import inspect
 from pogym.envs.blackjack import BlackJack
 from pogym.envs.higher_lower import HigherLower
+from pogym.envs.stateless_cartpole import StatelessCartPole
+from pogym.envs.stateless_pendulum import StatelessPendulum
 
 ALL_ENVS = {
-        BlackJack: {
-            "id": "pogym-Blackjack-v0",
-        },
-        HigherLower: {
-            "id": "pogym-HigherLower-v0"
-        },
+    BlackJack: {
+        "id": "pogym-Blackjack-v0",
+    },
+    HigherLower: {"id": "pogym-HigherLower-v0"},
+    StatelessCartPole: {"id": "pogym-StatelessCartPole-v0"},
+    StatelessPendulum: {
+        "id": "pogym-StatelessPendulum-v0",
+    },
 }
 
 for e, v in ALL_ENVS.items():
     gym.envs.register(
-        entry_point=":".join([inspect.getmodule(e).__name__, e.__name__]),
-        **v
+        entry_point=":".join([inspect.getmodule(e).__name__, e.__name__]), **v
     )
---
+++ pogym/envs/stateless_pendulum.py
@@ -0,0 +1,47 @@
+# Inspired by ray rllib at
+# https://github.com/ray-project/ray/blob/master/rllib/examples/env/stateless_pendulum.py
+
+from typing import Tuple, Union, Optional
+from gym.spaces import Box
+import numpy as np
+import gym
+
+from gym.envs.classic_control import PendulumEnv
+
+
+class StatelessPendulum(PendulumEnv):
+    """Partially observable variant of the Pendulum gym environment.
+    https://github.com/openai/gym/blob/master/gym/envs/classic_control/
+    pendulum.py
+    We delete the angular velocity component of the state, so that it
+    can only be solved by a memory enhanced model (policy).
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Fix our observation-space (remove angular velocity component).
+        high = np.array([1.0, 1.0], dtype=np.float32)
+        self.observation_space = Box(low=-high, high=high, dtype=np.float32)
+
+    def step(
+        self, action: gym.core.ActType
+    ) -> Tuple[gym.core.ObsType, float, bool, dict]:
+        next_obs, reward, done, info = super().step(action)
+        # next_obs is [cos(theta), sin(theta), theta-dot (angular velocity)]
+        return next_obs[:-1], reward, done, info
+
+    def reset(
+        self,
+        *,
+        seed: Optional[int] = None,
+        return_info: bool = False,
+        options: Optional[dict] = None
+    ) -> Union[gym.core.ObsType, Tuple[gym.core.ObsType, dict]]:
+        if return_info:
+            init_obs, info = super().reset()
+            return init_obs[:-1], info
+
+        init_obs = super().reset()
+        # init_obs is [cos(theta), sin(theta), theta-dot (angular velocity)]
+        return init_obs[:-1]
